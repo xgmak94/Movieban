84694d26cb29ad71afff138aebc3e952d61a2657
xgmak94 <xgmak94@gmail.com>
2022-11-13 23:05:02 -0800

    Fixed modal closing

--- components/Card/Modal/MovieInfoModal.tsx
+++ components/Card/Modal/MovieInfoModal.tsx
@@ -9,19 +9,23 @@ interface Props {
 }
 
 export default function MovieInfoModal({ movie, index, setModal }: Props) {
+  function handleClick(e: any) {
+    if (e.target.id === 'modal-background') {
+      setModal((prev) => !prev);
+    }
+  }
+
   return (
     <>
       <div
         className="grid place-items-center fixed top-0 left-0 w-screen h-screen bg-slate-100/40 z-10 cursor-auto"
         id="modal-background"
-        onClick={() => setModal((prev) => !prev)}
+        onClick={handleClick}
       >
         <div
           className="flex flex-col w-[90vw] h-[90vh] p-5 rounded-lg gap-1 space-evenly
             bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100
           dark:from-gray-300 dark:via-gray-600 dark:to-blue-900"
-          id="modal-content"
-          onClick={(e) => e.stopPropagation()}
         >
           <ModalContent movie={movie} />
         </div>
--- components/Nav/ModeSwitch.tsx
+++ components/Nav/ModeSwitch.tsx
@@ -14,11 +14,11 @@ export default function ModeSwitch() {
   return (
     <div className="flex flex-col items-center justify-center overflow-hidden">
       <div className="flex">
-        <label className="inline-flex relative items-center mr-5 cursor-pointer">
+        <label className="inline-flex relative items-center cursor-pointer">
           <input type="checkbox" className="sr-only peer" checked={enabled} readOnly />
           <div
             onClick={handleToggle}
-            className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-600"
+            className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-600"
           />
           <span className="ml-2 text-sm font-medium cursor-default">
             {theme === 'light' ? <MdOutlineModeNight /> : <MdOutlineLightMode />}

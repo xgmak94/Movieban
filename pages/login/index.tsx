781082be49b70f6ffdfc5b4176ce6862eadcaf05
xgmak94 <xgmak94@gmail.com>
2023-03-26 19:23:47 -0700

    Fixing redirect

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { type NextRouter, useRouter } from 'next/router';
-import { type SupabaseClient, useSupabaseClient } from '@supabase/auth-helpers-react';
+import {
+  type SupabaseClient,
+  useSupabaseClient,
+} from '@supabase/auth-helpers-react';
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { useTheme } from 'next-themes';
 import type { UseThemeProps } from 'next-themes/dist/types';
+import type { GetServerSideProps } from 'next';
+import { RedirectTo } from '@supabase/auth-ui-react/dist/esm/src/types';
 
-export default function Login() {
+type ServeSideProps = { host: string | null };
+
+interface Props {
+  host: string | null;
+}
+
+export const getServerSideProps: GetServerSideProps<ServeSideProps> = async (
+  context
+) => ({ props: { host: context.req.headers.host || null } });
+
+export default function Login({ host }: Props) {
   const router: NextRouter = useRouter();
   const { theme, setTheme }: UseThemeProps = useTheme();
   const supabaseClient: SupabaseClient = useSupabaseClient();
@@ -18,16 +33,16 @@ export default function Login() {
 
   return (
     <div
-      className="min-h-screen p-3 text-black dark:text-white bg-gradient-to-tr
+      className='min-h-screen p-3 text-black dark:text-white bg-gradient-to-tr
     from-red-200 via-gray-200 to-blue-500
-    dark:from-gray-400 dark:via-gray-600 dark:to-blue-900"
+    dark:from-gray-400 dark:via-gray-600 dark:to-blue-900'
     >
       <Auth
-        redirectTo={process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}
+        redirectTo={host as RedirectTo}
         appearance={{ theme: ThemeSupa }}
         supabaseClient={supabaseClient}
         providers={['google', 'github']}
-        socialLayout="vertical"
+        socialLayout='vertical'
         theme={theme}
         localization={{ lang: 'en' }}
       />
